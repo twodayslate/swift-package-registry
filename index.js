ddf0dd01d89e9c852a6e4b6e4862192dd8a3fd5a
Mahek Adhaduk <mahekadhaduk007@gmail.com>
2023-03-15 13:40:34 +0530

    path "/" redirect fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const apicache = require('apicache')
  * @param {import('probot').Application} app
  */
 module.exports = async (app, { getRouter }) => {
-  const router = getRouter("/");
+  const router = getRouter(process.env.APP_NAME);
   // Your code here
   app.log('Yay, the app was loaded!')
 
